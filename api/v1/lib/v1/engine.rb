be6061a58c37e0db4766b004f3bc3325edbf849f
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2012-09-12 02:01:26 +0200

    Add V1 Tire config initializer

--- api/v1/lib/v1/engine.rb
+++ api/v1/lib/v1/engine.rb
@@ -5,8 +5,9 @@ module V1
     # prefix route helper functions with v1_api. (e.g. v1_api.root_path)
     engine_name 'v1_api'
 
-    initializer "v1.get_search_endpoint" do
-      V1.config.search_endpoint = V1::Search.get_search_endpoint
+    initializer "v1.tire_config" do
+      Tire::Configuration.url(V1::Search.get_search_endpoint)
+      Tire::Model::Search.index_prefix("test_") if Rails.env.test?
     end
 
   end
--- config/initializers/api_role.rb
+++ config/initializers/api_role.rb
@@ -1,5 +1,6 @@
 require 'dpla'
 
+#TODO: Move this to Dpla.config
 Dpla::API_ROLE = Dpla.get_api_role
 
 if Dpla::API_ROLE == Dpla::API_ROLE_PRODUCTION && %w(test development).include?(Rails.env)
