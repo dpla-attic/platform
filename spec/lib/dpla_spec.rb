538e2335c618f2b5057b9e658758e1f87a09f697
forgotpw1 <mrbarrettgriffith@gmail.com>
2012-09-11 23:25:06 -0600

    Update spec/lib/dpla_spec.rb
    fixed duplicate spec title in #checkin_config context

--- spec/lib/dpla_spec.rb
+++ spec/lib/dpla_spec.rb
@@ -32,7 +32,7 @@ describe Dpla do
       check.should == false
     end
 
-    it "should return false if it cannot find a given config file" do
+    it "should return true if it can find a given config file" do
       File.stub(:exists? ) {true}
       check = Dpla.check_config( __FILE__, %w( config/file.yml ) )
       check.should == true
