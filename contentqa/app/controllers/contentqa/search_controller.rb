50fddf62d05e6ed08f95607f136e35e67c9a48d2
Jeffrey Licht <jeff@podconsulting.com>
2012-12-11 23:14:40 -0500

    Basic search interface

---
+++ contentqa/app/controllers/contentqa/search_controller.rb
@@ -0,0 +1,44 @@
+require_dependency "contentqa/application_controller"
+require "httparty"
+
+module Contentqa
+  class SearchController < ApplicationController
+    include HTTParty
+    PARAMETERS = %w[title description creator type publisher format rights contributor created spatial temporal source id q page_size page ]
+    DISPLAY = %w[id title description creator type publisher format rights contributor created spatial temporal source ingestDate]
+
+    def index
+      baseuri = request.protocol+request.host_with_port() 
+      api_params = params.delete_if { |key,value| !PARAMETERS.include? key }
+      json = self.class.get(baseuri+v1_api.items_path(api_params)).body
+      search_result = JSON.parse(json)
+
+      @count, @start,  @limit = search_result['count'], search_result['start'], search_result['limit']
+      @results = search_result['docs']
+      @display_fields = DISPLAY
+      @page_size = params['page_size'].nil? || params['page_size'] == 0 ? 10 : params['page_size'].to_i
+      @page_list = paginate(@count, @start, @limit, @page_size )
+      @page_count = get_page_count(@count, @page_size)
+    end
+
+    def get_page_count (count, page_size)
+      count / page_size + (count % page_size > 0 ? 1 : 0) 
+    end
+
+    def paginate (count, start, limit, page_size)
+      page_count = get_page_count count, page_size
+      current_page = start / page_size + (start % page_size > 0 ? 1 : 0) 
+      if count == 0 
+        return (), 0
+      end
+      if (current_page < 10)
+        (1..[page_count,20].min)
+      else
+        ([1,current_page - 10].max..[page_count,current_page + 10].min)
+      end
+
+    end
+      
+
+  end
+end
--- contentqa/config/routes.rb
+++ contentqa/config/routes.rb
@@ -1,6 +1,9 @@
 Contentqa::Engine.routes.draw do
+  get "search/index"
+
   # All these routes are relative to this engine's mount point defined in the main app
 
   get "/compare" => "compare#index"
   get "/compare/:id" => "compare#index"
+  get "/search" => "search#index"
 end
