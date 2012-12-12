04954e8c24b47fa7563cb1a7a86484543d425e23
Jeffrey Licht <jeff@podconsulting.com>
2012-12-12 12:30:51 -0500

    Search by specific fields in content qa front-end

--- contentqa/app/assets/javascripts/contentqa/search.js
+++ contentqa/app/assets/javascripts/contentqa/search.js
@@ -1,2 +1,4 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(".search-fields").each(function(index, e) { if ($(e).find("input").val() != '') $(e).addClass("in"); });
+console.log($(".search-fields"));
--- contentqa/app/controllers/contentqa/search_controller.rb
+++ contentqa/app/controllers/contentqa/search_controller.rb
@@ -4,18 +4,21 @@ require "httparty"
 module Contentqa
   class SearchController < ApplicationController
     include HTTParty
-    PARAMETERS = %w[title description creator type publisher format rights contributor created spatial temporal source id q page_size page ]
+    PARAMETERS = %w[title description creator type publisher format rights contributor created spatial temporal.after temporal.before source id q page_size page ]
     DISPLAY = %w[id title description creator type publisher format rights contributor created spatial temporal source ingestDate]
-
+    SEARCH = %w[id title description creator type publisher rights contributor spatial temporal.after temporal.before]
     def index
       baseuri = request.protocol+request.host_with_port() 
-      api_params = params.delete_if { |key,value| !PARAMETERS.include? key }
+      api_params = params.delete_if { |key,value| !PARAMETERS.include? key}
+      api_params = api_params.delete_if { |key,value| value.nil? || value.empty?}
+
       json = self.class.get(baseuri+v1_api.items_path(api_params)).body
       search_result = JSON.parse(json)
 
       @count, @start,  @limit = search_result['count'], search_result['start'], search_result['limit']
       @results = search_result['docs']
       @display_fields = DISPLAY
+      @search_fields = SEARCH
       @page_size = params['page_size'].nil? || params['page_size'] == 0 ? 10 : params['page_size'].to_i
       @page_list = paginate(@count, @start, @limit, @page_size )
       @page_count = get_page_count(@count, @page_size)
