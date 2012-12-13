6124a4c2afd701690fe6fb48ce1c77d9329b1414
Jeffrey Licht <jeff@podconsulting.com>
2012-12-13 15:13:49 -0500

    Add links from search back to compare

--- contentqa/app/controllers/contentqa/compare_controller.rb
+++ contentqa/app/controllers/contentqa/compare_controller.rb
@@ -4,26 +4,33 @@ require "httparty"
 module Contentqa
   class CompareController < ApplicationController
     include HTTParty
+
     def index
       baseuri = request.protocol+request.host_with_port() 
-      if true or params[:id] == nil
-        all_items_json = self.class.get(baseuri+v1_api.items_path()).body
-        all_items = JSON.parse(all_items_json)
-        @total = all_items['count'] 
-        @page = rand(all_items['count'])
-        target_item_json = self.class.get(baseuri+v1_api.items_path('page_size' => '1', 'page' => @page)).body
-        target_item = JSON.parse(target_item_json)      
-        doc = target_item['docs'][0]
+      all_items_json = self.class.get(baseuri+v1_api.items_path()).body
+      all_items = JSON.parse(all_items_json)
+      @total = all_items['count'] 
+      if params[:id] == nil
+        doc = get_random_doc(baseuri, all_items['count'])
       else
         target_item_json = self.class.get(baseuri+v1_api.items_path()+'/'+params[:id]).body
         target_item = JSON.parse(target_item_json)      
-        doc = target_item['doc']
+        doc = target_item[0]['doc']
       end
       
       @original = JSON.pretty_generate(doc['dplaSourceRecord'])
       doc.delete_if { |key,_| ['dplaSourceRecord','@context','_rev','_id'].include? key}
       @twisted = JSON.pretty_generate(doc)
       @link = baseuri+v1_api.items_path()+'/'+doc['id'] 
+      @next = get_random_doc(baseuri, all_items['count'])['id']
     end
+
+    def get_random_doc(baseuri, count)
+        @page = rand(count)
+        target_item_json = self.class.get(baseuri+v1_api.items_path('page_size' => '1', 'page' => @page)).body
+        target_item = JSON.parse(target_item_json)      
+        doc = target_item['docs'][0]
+    end
+
   end
 end
