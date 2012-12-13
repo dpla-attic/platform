f8193941a6b0e8fc159e1cf981a2547b5894e118
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2012-12-12 22:13:13 -0500

    Handle connection-refused exceptions gracefully

--- v1/app/controllers/v1/application_controller.rb
+++ v1/app/controllers/v1/application_controller.rb
@@ -7,15 +7,10 @@ module V1
     end
 
     def render_and_return_status_code(code)
-
       valid_codes = %w(200 400 401 404 406 429 500 503)
-
       status = valid_codes.include?(code) ? code : 500
-      
       render :text => "Raised Mock Error", :status => status 
-
       return false
-    
     end
   end
 end
