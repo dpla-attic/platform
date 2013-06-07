e99987c33aa297adaaadbae47890c4b39ef87908
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2013-06-07 16:51:35 -0400

    Return valid JSON for all use cases

--- features/step_definitions/format_results_steps.rb
+++ features/step_definitions/format_results_steps.rb
@@ -1,8 +1,25 @@
 When /^I pass callback param "(.*?)" to a search for "(.*?)"$/ do |callback, keyword|
+  @callback = callback
   @params.merge!({ 'q' => keyword, 'callback' => callback })
 end
 
+Then(/^I should get a valid JSON response$/) do
+  resource_query(@resource, @params, false)
+  json = page.source
+
+  if @callback
+    json =~ /^#{@callback}\((.+)\)$/m
+    json = $1
+  end
+  
+  expect(json).not_to be_nil
+  expect {
+    JSON.parse(json) 
+  }.not_to raise_error JSON::ParserError
+end
+
 Then /^the API response should start with "(.*?)"$/ do |callback|
-  item_query(@params)
+  @resource = 'item'
+  resource_query(@resource, @params)
   expect(page.source).to match /^#{callback}\(/
 end
