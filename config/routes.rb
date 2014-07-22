dfddf80bfa191efc245a6e735471179b7888d73c
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2014-07-22 14:23:20 -0400

    Redirect / to the public facing dp.la site

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Dpla::Application.routes.draw do
 
   devise_for :users
 
-  root :to => "home#index"
+  root :to => redirect('http://dp.la/info/developers/codex/')
 
   mount V1::Engine => "/v2"
   mount Contentqa::Engine => "/qa"
