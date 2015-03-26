85e63bb28948be3ed8a85911ffd18e4aa9399640
Mark Breedlove <mb@dp.la>
2015-03-26 17:23:29 -0400

    Update to use ElasticSearch for item "fetch"
    Remove query that used CouchDB for specific-item fetches.  Use
    `V1::Searchable#search` instead.

--- v1/lib/v1/searchable.rb
+++ v1/lib/v1/searchable.rb
@@ -213,39 +213,29 @@ module V1
       end
     end
 
-    def id_to_private_id(ids)
-      #TODO: use a cacheable filter here instead?
-      search_params = {'id' => ids.join(' OR '), 'page_size' => 50}
-      search(search_params)['docs'].inject({}) do |memo, doc|
-        memo[doc['id']] = doc['_id']
-        memo
-      end
-    end
-
     def fetch(ids)
-      # Transparently translate "id" values from query to the "_id" values CouchDB expects
-      # Accepts an array of ids or a string containing a comma separated list of ids
+      # Accepts an array of ids OR a string containing a comma-separated list
+      # of ids
       ids = ids.split(/,\s*/) if ids.is_a?(String)
 
-      id_map = id_to_private_id(ids)
+      fetches = search({'id' => ids.join(' OR '), 'page_size' => 50})
 
-      # Business logic: if they fetched one doc and it was 404, raise.
-      if id_map.empty? && ids.size == 1
+      found_ids = []
+      docs = fetches['docs'].map do |doc|
+        found_ids << doc['id']
+        doc
+      end
+      misses = ids - found_ids
+      if ids.size == 1 && misses.size == 1
         raise NotFoundSearchError, "Document not found"
       end
-
-      fetches = Repository.fetch(id_map.values)
-      # fetched docs that were deleted in the repo will have a nil 'doc' field
-      fetches['docs'].delete_if {|doc| doc.nil?}
-
-      misses = ids - fetches['docs'].map {|doc| doc['id']}
       misses.each do |id|
-        fetches['docs'] << { 'id' => id, 'error' => '404' }
+        docs << {'id' => id, 'error' => '404'}
       end
 
       {
-        'docs' => fetches['docs'],
-        'count' => fetches['docs'].size
+        'docs' => docs,
+        'count' => docs.size
       }
     end
 
